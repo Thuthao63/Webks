ab135800eb8f84a8db0041b89e7f3edd803fa20e
Nguyen Thi Thu Thao <nttt603204@gmail.com>
2026-06-07 22:14:51 +0700

    update code

--- backend/scripts/test_users.js
+++
@@ -1,8 +0,0 @@
-const User = require('../models/User');
-
-async function test() {
-    const users = await User.findAll({ attributes: ['id', 'email', 'fullName'] });
-    console.log(users.map(u => u.toJSON()));
-}
-
-test();
